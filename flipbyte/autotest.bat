71205b3ed1f8801d3a502923efb8cfbe701ab03a
hntsk <nikita.avdeev.06@bk.ru>
2026-02-18 22:45:30 +0300

    Add files via upload

---
+++ flipbyte/autotest.bat
@@ -0,0 +1,36 @@
+@echo off 
+chcp 1251 > nul
+
+echo Тест 1
+flipbyte.exe 6 | find "96"
+if errorlevel 1 (
+    echo Ожидаемый вывод 96 не найден
+) else (
+    echo Найдено 96
+)
+
+echo Тест 2
+flipbyte.exe -12 | find "Присутствуют посторонние символы, помимо цифр"
+if errorlevel 1 (
+    echo Ожидаемый вывод не найден
+) else (
+    echo Найден ожидаемый вывод
+)
+
+echo Тест 3
+flipbyte.exe 260 | find "Число больше 255"
+if errorlevel 1 (
+    echo Ожидаемый вывод не найден
+) else (
+    echo Найден ожидаемый вывод
+)
+
+echo Тест 4
+flipbyte.exe -12 abc | find "Неверное количество входных данных"
+if errorlevel 1 (
+    echo Ожидаемый вывод не найден
+) else (
+    echo Найден ожидаемый вывод
+)
+
+pause
